67e5207429d34b53191b6556b293fdb2a1395fd1
unknown <abaloslyne@gmail.com>
2022-09-22 00:45:47 +0800

    -batch upload

---
+++ push.bat
@@ -0,0 +1,8 @@
+@echo off
+git remote set-url origin https://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@github.com/Jeshlyne/cit14-16.git
+pause
+git add .
+git commit -m -"batch upload"
+git push
+pause
+
